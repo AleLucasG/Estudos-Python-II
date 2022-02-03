a91acdb3aeb46f0af78079681ced3aa54e538993
Alessandra Lucas <alessandra1702@hotmail.com>
2022-02-03 13:43:53 -0300

    Update ex038.py

--- ex038.py
+++ ex038.py
@@ -2,3 +2,13 @@
 – O primeiro valor é maior;
 – O segundo valor é maior;
 – Não existe valor maior, os dois são iguais"""
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print('\033[1;33mO primeiro número digitado foi {} e ele tem valor maior do que {} \033[m'.format(n1, n2))
+elif n2 > n1:
+    print('\033[1;34mO segundo número digitado foi {} e ele tem valor maior do que {} \033[m'.format(n2, n1))
+else:
+    print('\033[1;35mNão existe valor maior, os dois são iguais \033[m')
